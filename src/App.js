5210e6e2d6f4fcd8678341d5ce406fa50f6aea9d
swaraj <swarajdeshmukh45@gmail.com>
2021-05-09 17:48:10 +0530

    ConditionalRendering - Day2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import {UserDetails} from './UserDetails'
 import EventHandling from './EventHandling'
+import ConditionalRendering from './ConditionalRendering'
 
 function App() {
-    console.log(<h1>Hi</h1>)
-    let name = "Swaraj";
-    const displayName = (name)=>{
-      alert("Hi" + name);
-    }
+    // console.log(<h1>Hi</h1>)
+    // let name = "Swaraj";
+    // const displayName = (name)=>{
+    //   alert("Hi" + name);
+    // }
 
   return (
     <div className="App">
-      <h1>Hello {name}</h1>
-      <button onClick></button>
+
+      <ConditionalRendering isLoggedIn={true} />
+
+      {/* <h1>Hello {name}</h1> */}
+      {/* <button onClick></button>
       <UserDetails name="John" age={21} displayName = {displayName}>
         <div>
           <h1>This information is specific to John</h1>
         </div>
-      </UserDetails>
+      </UserDetails> */}
 
       {/* <EventHandling/> */}
 
--- src/ConditionalRendering.js
+++ src/ConditionalRendering.js
@@ -1,11 +1,50 @@
 import React from 'react'
 
-function ConditionalRendering() {
-    return (
-        <div>
-            
-        </div>
-    )
+function ConditionalRendering(isLoggedIn) {
+   
+// If the condition is true i want to render something but if it is false
+// i dont want to render anything 
+      return (
+          <div>
+              {isLoggedIn && <h1>You are logged in</h1>}
+          </div>
+      )  
+
+
+//    return (     Type 3 TernaryOPERATOR
+//        <div>
+//            {isLoggedIn ? <h1> You are logged in </h1> : <h1> Please log in </h1>}
+//        </div>
+//    )
+   
+   
+   
+    // let message;      Type 2 Element variable method
+    // if (isLoggedIn)
+    // {
+    //     message = <h1>You are logged in</h1>
+    // }
+    // else{
+    //     message = <h1>Please sign in</h1>
+    // }
+    // return (
+    //     <div>
+    //         {message}
+    //     </div>
+    // )
+    
+    // return (         Type 1 IfElse
+    //     <div>
+    //         <h1>You are logged in</h1>
+    //     </div>
+    // )
+    // else{
+    //     return (
+    //     <div>
+    //         <h1>Please log in</h1>
+    //     </div>
+    //     )
+    // }
 }
 
 export default ConditionalRendering
